7a0049d6db8d5b0228abce45ddb6f8792f495d9b
admin <artemsmolyaninov@gmail.com>
2024-01-23 18:22:26 +0300

    first use oop

---
+++ home.py
@@ -0,0 +1,13 @@
+class House:
+    def __init__(self, numberOfFloors=10):
+        self.numberOfFloors = numberOfFloors
+
+    def printFloors(self):
+        for floor in range(1, self.numberOfFloors + 1):
+            print("Текущий этаж равен", floor)
+
+
+
+myHouse = House()
+
+myHouse.printFloors()
