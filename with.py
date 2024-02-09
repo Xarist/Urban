9fce328cb88652c845c71b9e7cf6c3295811309b
admin <artemsmolyaninov@gmail.com>
2024-02-09 19:12:14 +0300

    first usage 'with'

--- with.py
+++ with.py
@@ -1,4 +1,6 @@
 my_file = 'file.txt'
-with open(my_file, 'r') as file:
+with open(my_file, 'r', encoding='utf8') as file:
     for line in file:
         print(line, end='')
+print()
+print('Файл закрыт') if file.closed==True else print('Файл все еще открыт')
