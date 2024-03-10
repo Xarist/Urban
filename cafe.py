ac96f0fb23afb82c527aeb6af288e3a8648adcbe
admin <artemsmolyaninov@gmail.com>
2024-03-10 22:17:23 +0300

    Cafe

--- cafe.py
+++ cafe.py
@@ -23,14 +23,14 @@ class Cafe:
                 return
 
         self.queue.put(customer_number)
-        print(f"Посетитель номер {customer_number} добавлен в очередь ожидания.", flush=True)
+        print(f"Посетитель номер {customer_number} ожидает свободный стол.", flush=True)
 
     def serve_customer(self, customer_number, current_table):
         current_table.is_busy = True
-        print(f"Посетитель номер {customer_number} занимает стол {current_table.number}.", flush=True)
-        time.sleep(5)  # время обслуживания
+        print(f"Посетитель номер {customer_number} сел за стол {current_table.number}.", flush=True)
+        time.sleep(5)
         current_table.is_busy = False
-        print(f"Посетитель номер {customer_number} закончил и ушёл.", flush=True)
+        print(f"Посетитель номер {customer_number} покушал и ушёл.", flush=True)
 
         if not self.queue.empty():
             next_customer = self.queue.get()
@@ -50,9 +50,10 @@ class Customer(threading.Thread):
         self.cafe.customer_arrival(self.customer_number)
 
 
-tables = [1, 2, 3]  # список номеров столов
+tables = [1, 2, 3]
 cafe = Cafe(tables)
 
-for i in range(10):  # количество посетителей
+for i in range(20):
     customer = Customer(cafe, i + 1)
     customer.start()
+    time.sleep(1)
